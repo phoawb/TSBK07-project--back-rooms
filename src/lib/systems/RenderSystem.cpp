1cb05525d6f4c7ecb24b8f3e02a879c81e5552b9
Philip Welin-Berger <philip.welin.berger@gmail.com>
2024-04-07 23:12:43 +0200

    Set starting position to the starting room facing out towards the corridor

--- src/lib/systems/RenderSystem.cpp
+++ src/lib/systems/RenderSystem.cpp
@@ -58,9 +58,9 @@ void RenderSystem::Init() {
 
   // Add camera and transform component to entity
   mCamera = gCoordinator.CreateEntity();
-  gCoordinator.AddComponent(mCamera, Transform{.position = vec3(-200.0f, 50.0f, 0.0f)});
+  gCoordinator.AddComponent(mCamera, Transform{.position = vec3(-85.0f, 10.0f, 80.0f)});
   gCoordinator.AddComponent(mCamera, Camera{.projectionTransform = projectionMatrix,
-                                            .theta = 0,
+                                            .theta = -M_PI_2,
                                             .phi = 0,
                                             .lookAt = vec3(0.0f, 50.0f, 0.0f),
                                             .cameraUp = vec3(0.0f, 1.0f, 0.0f)});
