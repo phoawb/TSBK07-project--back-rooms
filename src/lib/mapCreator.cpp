5bedaf04d99887954330b0353c1b784e5da49656
Philip Welin-Berger <philip.welin.berger@gmail.com>
2024-04-11 19:24:10 +0200

    make rand function exlusive of max value

--- src/lib/mapCreator.cpp
+++ src/lib/mapCreator.cpp
@@ -26,9 +26,10 @@ extern Coordinator gCoordinator;
 
 int randRange(int min, int max) {
   if (min > max) std::swap(min, max);
-  static std::random_device rd;                     // Obtain a random number from hardware
-  static std::mt19937 gen(rd());                    // Seed the generator
-  std::uniform_int_distribution<> distr(min, max);  // Define the range
+  if (min == max) return min;
+  static std::random_device rd;                         // Obtain a random number from hardware
+  static std::mt19937 gen(rd());                        // Seed the generator
+  std::uniform_int_distribution<> distr(min, max - 1);  // Define the range
 
   return distr(gen);
 }
