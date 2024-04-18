343d852d507b52552c4f54e00847c61cd8460b47
Philip Welin-Berger <philip.welin.berger@gmail.com>
2024-04-18 19:31:19 +0200

    add possibility of being inclusive

--- src/lib/mapGeneration/mapUtils.cpp
+++ src/lib/mapGeneration/mapUtils.cpp
@@ -3,12 +3,13 @@
 #include "algorithm"
 #include "random"
 
-int randRange(int min, int max) {
+int randRange(int min, int max, bool isInclusive) {
   if (min > max) std::swap(min, max);
   if (min == max) return min;
-  static std::random_device rd;                         // Obtain a random number from hardware
-  static std::mt19937 gen(rd());                        // Seed the generator
-  std::uniform_int_distribution<> distr(min, max - 1);  // Define the range
+  static std::random_device rd;   // Obtain a random number from hardware
+  static std::mt19937 gen(rd());  // Seed the generator
+  int distrMax = isInclusive ? max : max - 1;
+  std::uniform_int_distribution<> distr(min, distrMax);  // Define the range
 
   return distr(gen);
 }
