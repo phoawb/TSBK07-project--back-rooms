1a22226eb5da6a445d502a346ce6ace88b1a87cf
asoderlind <axesode@gmail.com>
2024-04-18 12:46:47 +0200

    remove unncecessary code

--- src/lib/systems/LightingSystem.cpp
+++ src/lib/systems/LightingSystem.cpp
@@ -8,28 +8,7 @@
 extern Coordinator gCoordinator;
 extern AssetManager assetManager;
 
-void LightingSystem::Init() {
-  int lightCount = mEntities.size();
-  vec3 lightSourcesColors[lightCount];
-  vec3 lightSourcesDirPos[lightCount];
-  int isDirectional[lightCount];
-  int i = 0;
-  for (auto const& entity : mEntities) {
-    auto& light = gCoordinator.GetComponent<Light>(entity);
-    auto& transform = gCoordinator.GetComponent<Transform>(entity);
-    lightSourcesColors[i] = light.color;
-    vec3 lightSourcePos = trans2pos(transform.translation);
-    lightSourcesDirPos[i] = lightSourcePos;
-    isDirectional[i] = light.isDirectional;
-    i++;
-  }
-  auto shaderId = assetManager.getShaderId(TERRAIN);
-  glUseProgram(shaderId);
-  glUniform1i(glGetUniformLocation(shaderId, "lightCount"), lightCount);
-  glUniform3fv(glGetUniformLocation(shaderId, "lightSourcesColors"), lightCount, &lightSourcesColors[0].x);
-  glUniform3fv(glGetUniformLocation(shaderId, "lightSourcesDirPos"), lightCount, &lightSourcesDirPos[0].x);
-  glUniform1iv(glGetUniformLocation(shaderId, "isDirectional"), lightCount, &isDirectional[0]);
-};
+void LightingSystem::Init() {};
 
 void LightingSystem::Update() {
   // random number between 0 and 1
